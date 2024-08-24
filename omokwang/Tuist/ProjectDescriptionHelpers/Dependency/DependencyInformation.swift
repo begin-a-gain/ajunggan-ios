d854dbf2c99f4570e6356c8323c20a22938d6091
kdjun97 <kdjun97@gmail.com>
2024-08-24 15:49:26 +0900

    feat/#10: add data module

--- omokwang/Tuist/ProjectDescriptionHelpers/Dependency/DependencyInformation.swift
+++ omokwang/Tuist/ProjectDescriptionHelpers/Dependency/DependencyInformation.swift
@@ -9,13 +9,15 @@ import Foundation
 
 let dependencyInfo: [DependencyInformation: [DependencyInformation]] = [
     .App: [.SignIn],
-    .Domain: []
+    .Domain: [],
+    .Data: [.Domain]
 ]
 
 public enum DependencyInformation: String {
     case App = "App"
     case SignIn = "SignIn"
     case Domain = "Domain"
+    case Data = "Data"
 }
 
 public enum PresentationDependencyInformation: String, CaseIterable {
