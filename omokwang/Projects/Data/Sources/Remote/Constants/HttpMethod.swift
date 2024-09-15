ffdcc4d9ec824f087631bdcd2101dfc4746703c5
kdjun97 <kdjun97@gmail.com>
2024-09-15 16:30:35 +0900

    feat/#29: add ApiService for network

---
+++ omokwang/Projects/Data/Sources/Remote/Constants/HttpMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HttpMethod.swift
+//  Data
+//
+//  Created by 김동준 on 9/15/24
+//
+
+public enum HttpMethod: String {
+    case GET = "GET"
+    case POST = "POST"
+    case DELETE = "DELETE"
+    case PUT = "PUT"
+}
