01ee2b844927fc5ec74fb19e37699be6e50d600a
kdjun97 <kdjun97@gmail.com>
2024-11-24 01:59:47 +0900

    feat/#118: add AlertFeature

---
+++ omokwan/Projects/Presentation/Base/Sources/Extension/View+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  View+Extension.swift
+//  Base
+//
+//  Created by 김동준 on 11/24/24
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+public extension View {
+    @ViewBuilder
+    func oAlert(
+        _ store: StoreOf<AlertFeature>,
+        content: @escaping () -> some View
+    ) -> some View {
+        self.modifier(
+            AlertViewModifier(
+                viewStore: ViewStore(store, observe: { $0 }),
+                alertContent: content
+            )
+        )
+    }
+}
