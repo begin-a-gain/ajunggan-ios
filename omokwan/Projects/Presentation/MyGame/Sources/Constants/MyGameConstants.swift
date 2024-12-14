2928f1530de5d6cf7668a7b0992748f6350d537c
kdjun97 <kdjun97@gmail.com>
2024-12-14 16:29:58 +0900

    design/#137: add model and dependency

---
+++ omokwan/Projects/Presentation/MyGame/Sources/Constants/MyGameConstants.swift
@@ -0,0 +1,15 @@
+//
+//  MyGameConstants.swift
+//  MyGame
+//
+//  Created by 김동준 on 12/7/24
+//
+
+import Foundation
+
+struct MyGameConstants {
+    static let stonePadding: CGFloat = 16
+    static let linearGradientStartPoint: CGFloat = 0.00
+    static let linearGradientMiddlePoint: CGFloat = 0.67
+    static let linearGradientEndPoint: CGFloat = 1.00
+}
